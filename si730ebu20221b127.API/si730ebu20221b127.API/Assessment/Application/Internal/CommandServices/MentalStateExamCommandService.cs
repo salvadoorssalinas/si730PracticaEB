02ecbb6812bac9d02dd7f4fe76e52ea1f788b06a
salvadoorssalinas <u20221b127@upc.edu.pe>
2024-07-02 20:50:39 -0500

    feat: validations for mental state exams

--- si730ebu20221b127.API/si730ebu20221b127.API/Assessment/Application/Internal/CommandServices/MentalStateExamCommandService.cs
+++ si730ebu20221b127.API/si730ebu20221b127.API/Assessment/Application/Internal/CommandServices/MentalStateExamCommandService.cs
@@ -21,6 +21,38 @@ public class MentalStateExamCommandService(IMentalStateExamRepository mentalStat
             Console.WriteLine("Examiner not found");
             return null;
         }
+        // check if date is in the future
+        if (command.ExamDate > DateTime.Now)
+        {
+            Console.WriteLine("Exam date cannot be in the future.");
+            return null;
+        }
+        // check score values
+        if (command.OrientationScore < 0 || command.OrientationScore > 10)
+        {
+            Console.WriteLine("Orientation score must be between 0 and 10.");
+            return null;
+        }
+        if (command.RegistrationScore < 0 || command.RegistrationScore > 3)
+        {
+            Console.WriteLine("Registration score must be between 0 and 3.");
+            return null;
+        }
+        if (command.AttentionAndCalculationScore < 0 || command.AttentionAndCalculationScore > 5)
+        {
+            Console.WriteLine("Attention and calculation score must be between 0 and 5.");
+            return null;
+        }
+        if (command.RecallScore < 0 || command.RecallScore > 3)
+        {
+            Console.WriteLine("Recall score must be between 0 and 3.");
+            return null;
+        }
+        if (command.LanguageScore < 0 || command.LanguageScore > 9)
+        {
+            Console.WriteLine("Language score must be between 0 and 9.");
+            return null;
+        }
         var examDate = new ExamDate(command.ExamDate);
         var mentalStateExam = new MentalStateExam(
             command.PatientId,
